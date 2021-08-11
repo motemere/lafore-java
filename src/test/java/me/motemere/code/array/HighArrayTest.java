9546365dc7bf4dbfe33ec1b3793240a25ab87149
Mote Mere <motemere@gmail.com>
2021-08-12 01:15:54 +0300

    add display names for tests of High Array

--- src/test/java/me/motemere/code/array/HighArrayTest.java
+++ src/test/java/me/motemere/code/array/HighArrayTest.java
@@ -3,6 +3,7 @@ package me.motemere.code.array;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class HighArrayTest {
@@ -16,16 +17,19 @@ class HighArrayTest {
   }
 
   @Test
+  @DisplayName("Find value")
   void find() {
     assertTrue(arr.find(1));
   }
 
   @Test
+  @DisplayName("Insert value")
   void insert() {
     assertTrue(arr.insert(1));
   }
 
   @Test
+  @DisplayName("Delete value")
   void delete() {
     assertTrue(arr.delete(2));
   }

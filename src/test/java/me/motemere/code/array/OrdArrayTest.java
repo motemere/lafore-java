3c07546b0d6de7863301257abc6e775560104312
Mote Mere <motemere@gmail.com>
2021-09-15 21:26:55 +0300

    upd Ordered Array

--- src/test/java/me/motemere/code/array/OrdArrayTest.java
+++ src/test/java/me/motemere/code/array/OrdArrayTest.java
@@ -3,36 +3,36 @@ package me.motemere.code.array;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("MagicNumber")
 class OrdArrayTest {
 
-  private OrdArray array;
+  private long[] nominalArray;
+  private OrdArray ordArray;
 
   @BeforeEach
   void setUp() {
-    long[] newArray = {0, 1, 2, 1, 0};
-    array = new OrdArray(newArray);
+    nominalArray = new long[]{1, 3, 4, 6, 6, 8, 9, 12, 17, 22, 46, 55, 73};
+
+    long[] newArray = {1, 6, 3, 8, 12, 55, 73, 9, 4, 6, 17, 22, 46};
+    ordArray = new OrdArray(newArray);
   }
 
-  @SuppressWarnings("MagicNumber")
   @Test
   void findValueByBinarySearch() {
-    long[] arr = {1, 6, 3, 8, 12, 55, 73, 9, 4, 6, 17, 22, 46};
-    OrdArray ordArray = new OrdArray(arr);
-    assertEquals(ordArray.find(55L), 11);
+    assertEquals(ordArray.find(55L), Arrays.binarySearch(ordArray.getArr(), 55L));
   }
 
   @Test
   void getArr() {
-    long[] arr = {0, 0, 1, 1, 2};
-    assertArrayEquals(array.getArr(), arr);
+    assertArrayEquals(ordArray.getArr(), nominalArray);
   }
 
   @Test
   void getLen() {
-    long[] arr = {0, 0, 1, 1, 2};
-    assertEquals(array.getLen(), arr.length);
+    assertEquals(ordArray.getLen(), nominalArray.length);
   }
 }

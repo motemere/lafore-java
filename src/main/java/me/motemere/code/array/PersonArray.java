c1ad67b1a0f252dc9687896d5ad25b7616d07a71
Mote Mere <motemere@gmail.com>
2021-09-15 23:41:09 +0300

    add PersonArray

---
+++ src/main/java/me/motemere/code/array/PersonArray.java
@@ -0,0 +1,111 @@
+package me.motemere.code.array;
+
+import java.util.Arrays;
+import me.motemere.code.model.Person;
+
+/**
+ * Array data structure for Person.
+ */
+public class PersonArray {
+
+  private Person[] arr;
+  private int len;
+
+  public PersonArray(Person[] personArray) {
+    this.arr = personArray;
+    this.len = personArray.length;
+  }
+
+  /**
+   * Find Person in array.
+   *
+   * @param anotherPerson Person
+   * @return result boolean
+   */
+  public boolean findPerson(Person anotherPerson) {
+    for (int i = 0; i < len; i++) {
+      if (arr[i].hashCode() == anotherPerson.hashCode()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Find Person by name in array.
+   *
+   * @param name String
+   * @return result boolean
+   */
+  public boolean findPersonByLastName(String name) {
+    for (int i = 0; i < len; i++) {
+      if (arr[i].getLastName().equals(name)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Insert new person to array.
+   *
+   * @param person Person
+   */
+  public void insert(Person person) {
+    Person[] newArray = new Person[len + 1];
+    System.arraycopy(arr, 0, newArray, 0, len);
+
+    newArray[len] = person;
+    arr = newArray;
+
+    len++;
+  }
+
+  /**
+   * Delete Person from array.
+   *
+   * @param name String
+   * @return result boolean
+   */
+  public boolean deletePersonByLastName(String name) {
+    Person[] newArray = new Person[len - 1];
+
+    if (newArray.length == 0) {
+      arr = newArray;
+      len = newArray.length;
+      return true;
+    }
+
+    int targetIdx = 0;
+
+    for (int i = 0; i < len; i++) {
+      if (arr[i].getLastName().equals(name)) {
+        continue;
+      }
+      newArray[targetIdx] = arr[i];
+      targetIdx++;
+    }
+
+    arr = newArray;
+    len--;
+
+    return !findPersonByLastName(name);
+  }
+
+  /**
+   * Display personArray contents.
+   *
+   * @return String contents
+   */
+  @Override
+  public String toString() {
+    return "PersonArray{"
+        + "arr="
+        + Arrays.toString(arr)
+        + ", len="
+        + len
+        + '}';
+  }
+}

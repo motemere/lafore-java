caf047cfdae1c279cf985b2f1aed35bd99d9ecb0
motemere <motemere@gmail.com>
2023-03-05 17:31:54 +0600

    primitive KeyValue

---
+++ src/test/java/me/motemere/code/store/KeyValueTest.java
@@ -0,0 +1,44 @@
+package me.motemere.code.store;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KeyValueTest {
+
+  private KeyValue keyValue;
+
+  @BeforeEach
+  void setUp() {
+    keyValue = new KeyValue("key", "value");
+  }
+
+  @Test
+  void getKey() {
+    assertEquals("key", keyValue.getKey());
+  }
+
+  @Test
+  void getValue() {
+    assertEquals("value", keyValue.getValue());
+  }
+
+  @Test
+  void setKey() {
+    keyValue.setKey("newKey");
+    assertEquals("newKey", keyValue.getKey());
+  }
+
+  @Test
+  void setValue() {
+    keyValue.setValue("newValue");
+    assertEquals("newValue", keyValue.getValue());
+  }
+
+  @Test
+  void testToString() {
+    assertEquals("KeyValue{key='key', value='value'}", keyValue.toString());
+  }
+
+}
